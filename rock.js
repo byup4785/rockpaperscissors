194b2cddd5cb1e9710b68ea54db15634244eb202
Byung <byup4785@gmail.com>
2023-05-26 16:46:39 -0400

    added loops and DOM display

--- rock.js
+++ rock.js
@@ -1,19 +1,72 @@
-function playRound(){
-    //x is the player, y is the opponent.//
-    let x = Math.floor(Math.random()*3);
-    let y = Math.floor(Math.random()*3);
+let win = 0;
+let lose = 0;
+let draw = 0;
+//const container = document.querySelector('#container');
 
-    console.log(x);
-    console.log(y);
-    //0 = rock, 1 = paper, 2 = scissors//
+
+function playing(x, y){
+    console.log(x + "and" + y);
     if(x == y){
-        console.log("it's a draw");
+        //console.log("it's a draw");
+        draw++;
     }
-    else if(x < y || (x == 2 && y == 0)){
-        console.log("you lose");
+    else if(x == 0 && y == 2){
+        //console.log("you win");
+        win++;
     }
-    else if(x > y || (x == 0 && y == 2)){
-        console.log("you win");
+    else if(x == 2 && y == 0){
+        //console.log("you lose");
+        lose++;
     }
+    else if(x < y){
+        //console.log("you lose");
+        lose++;
+    }
+    else if(x > y){
+        //console.log("you win");
+        win++;
+    }
+    score();
+    if(win == 5){display("you won"); win = 0; lose = 0; draw = 0;}
+    if(lose == 5){display("you lose"); win = 0; lose = 0; draw = 0;}
+}
 
+function display(x){
+    const container = document.querySelector('#con');
+    const scores = document.createElement('div');
+    container.replaceChildren(scores);
+    //container.removeChild(container.firstChild);
+    scores.classList.add('content');
+    scores.textContent = x;
+}
+
+function init(){
+    const container = document.querySelector('#con');
+    const scores = document.createElement('div');
+    scores.classList.add('content');
+    container.appendChild(scores);
+    scores.textContent = "this is the score" + win + lose + draw;
+}
+
+function play(){
+    const h = document.querySelectorAll('button');
+    console.log("wat in the fuck");
+    h.forEach((butt) => {
+        butt.addEventListener('click', () => {playing(butt.id, Math.floor(Math.random()*3));})
+        console.log("yyeeye");
+    });
+    
+}
+
+function score(){
+    const container = document.querySelector('#con');
+    const scores = document.createElement('div');
+    container.replaceChildren(scores);
+    //container.removeChild(container.firstChild);
+    scores.classList.add('content');
+    scores.textContent = "this is the score" + win + lose + draw;
+    //container.appendChild(scores);
+    //const co = document.querySelector('#con');
+    //const c1 = console.dir(co.firstElementChild);
+    //c1.textContent = "new new"   
 }
